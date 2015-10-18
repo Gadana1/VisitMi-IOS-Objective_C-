bcf958996a8c79f8ec82d794586814c17a0b96f9
Samuel <Splendy1@samuels-mac.local>
2015-10-18 19:59:58 +0800

    Initial Commit

---
+++ VisitMi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VisitMi
+//
+//  Created by Samuel on 10/18/15.
+//  Copyright © 2015 Mi S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VisitMi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VisitMi
+//
+//  Created by Samuel on 10/18/15.
+//  Copyright © 2015 Mi S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
